64a1bf61b4a0123b7971ddded6eb27e4e55e5143
Rex Kerr <ichoran@gmail.com>
2015-02-28 01:35:29 -0800

    Unboxing of spliterators and streams.  Seems to work better on spliterator, and better when an implicit def is used to create it than when the value class is created explicitly (at least on -optimize).  Not sure why.

--- src/main/scala/Specializer.scala
+++ src/main/scala/Specializer.scala
@@ -15,7 +15,7 @@ package spec {
   }
   
   trait Unboxer[A, S[A], SU] {
-    def apply(original: S[A]): SU
+    def apply(boxed: S[A]): SU
   }
   
   final class RichObjectSpliterator[A](val underlying: Spliterator[A]) extends AnyVal {
@@ -51,7 +51,80 @@ package spec {
   
   final class RichLongStream(val underlying: LongStream) extends AnyVal {
   }
+  
+  final class UnboxedDoubleSpliterator(boxed: Spliterator[Double]) extends Spliterator.OfDouble {
+    override def characteristics() = boxed.characteristics()
+    override def estimateSize() = boxed.estimateSize()
+    override def forEachRemaining(action: function.Consumer[_ >: java.lang.Double]) = boxed.forEachRemaining(action.asInstanceOf[function.Consumer[_ >: Double]])
+    override def forEachRemaining(action: function.DoubleConsumer) = boxed.forEachRemaining(d => action.accept(d))
+    override def getComparator(): Comparator[_ >: java.lang.Double] = boxed.getComparator().asInstanceOf[Comparator[_ >: java.lang.Double]]
+    override def getExactSizeIfKnown() = boxed.getExactSizeIfKnown()
+    override def hasCharacteristics(characteristics: Int) = boxed.hasCharacteristics(characteristics)
+    override def tryAdvance(action: function.Consumer[_ >: java.lang.Double]) = boxed.tryAdvance(action.asInstanceOf[function.Consumer[_ >: Double]])
+    def tryAdvance(action: function.DoubleConsumer) = boxed.tryAdvance(d => action.accept(d))
+    override def trySplit() = boxed.trySplit() match { case null => null; case x => new UnboxedDoubleSpliterator(x) }
+  }
+  
+  final class UnboxedIntSpliterator(boxed: Spliterator[Int]) extends Spliterator.OfInt {
+    override def characteristics() = boxed.characteristics()
+    override def estimateSize() = boxed.estimateSize()
+    override def forEachRemaining(action: function.Consumer[_ >: java.lang.Integer]) { boxed.forEachRemaining(action.asInstanceOf[function.Consumer[_ >: Int]]) }
+    override def forEachRemaining(action: function.IntConsumer) = boxed.forEachRemaining(d => action.accept(d))
+    override def getComparator(): Comparator[_ >: java.lang.Integer] = boxed.getComparator().asInstanceOf[Comparator[_ >: java.lang.Integer]]
+    override def getExactSizeIfKnown() = boxed.getExactSizeIfKnown()
+    override def hasCharacteristics(characteristics: Int) = boxed.hasCharacteristics(characteristics)
+    override def tryAdvance(action: function.Consumer[_ >: java.lang.Integer]) = boxed.tryAdvance(action.asInstanceOf[function.Consumer[_ >: Int]])
+    def tryAdvance(action: function.IntConsumer) = boxed.tryAdvance(d => action.accept(d))
+    override def trySplit() = boxed.trySplit() match { case null => null; case x => new UnboxedIntSpliterator(x) }
+  }
+  
+  final class UnboxedLongSpliterator(boxed: Spliterator[Long]) extends Spliterator.OfLong {
+    override def characteristics() = boxed.characteristics()
+    override def estimateSize() = boxed.estimateSize()
+    override def forEachRemaining(action: function.Consumer[_ >: java.lang.Long]) = boxed.forEachRemaining(action.asInstanceOf[function.Consumer[_ >: Long]])
+    override def forEachRemaining(action: function.LongConsumer) = boxed.forEachRemaining(d => action.accept(d))
+    override def getComparator(): Comparator[_ >: java.lang.Long] = boxed.getComparator().asInstanceOf[Comparator[_ >: java.lang.Long]]
+    override def getExactSizeIfKnown() = boxed.getExactSizeIfKnown()
+    override def hasCharacteristics(characteristics: Int) = boxed.hasCharacteristics(characteristics)
+    override def tryAdvance(action: function.Consumer[_ >: java.lang.Long]) = boxed.tryAdvance(action.asInstanceOf[function.Consumer[_ >: Long]])
+    def tryAdvance(action: function.LongConsumer) = boxed.tryAdvance(d => action.accept(d))
+    override def trySplit() = boxed.trySplit() match { case null => null; case x => new UnboxedLongSpliterator(x) }
+  }
+  
+  trait Java8StreamImplicits2 {
+  }
+  trait Java8StreamImplicits1 extends Java8StreamImplicits2 {
+    implicit def enrichObjectSpliterator[A](sp: Spliterator[A]) = new RichObjectSpliterator(sp)
+    implicit def enrichObjectStream[A](os: ObjectStream[A]) = new RichObjectStream(os)
+  }
+  trait Java8StreamImplicits extends Java8StreamImplicits1 {
+    implicit def enrichDoubleSpliterator(spd: Spliterator.OfDouble) = new RichDoubleSpliterator(spd)
+    implicit def enrichIntSpliterator(spi: Spliterator.OfInt) = new RichIntSpliterator(spi)
+    implicit def enrichLongSpliterator(spl: Spliterator.OfLong) = new RichLongSpliterator(spl)
+    implicit def enrichDoubleStream(ds: DoubleStream) = new RichDoubleStream(ds)
+    implicit def enrichIntStream(is: IntStream) = new RichIntStream(is)
+    implicit def enrichLongStream(ls: LongStream) = new RichLongStream(ls)
+  
+    implicit val spliteratorUnboxDouble = new Unboxer[Double, Spliterator, Spliterator.OfDouble] {
+      def apply(boxed: Spliterator[Double]) = new UnboxedDoubleSpliterator(boxed)
+    }
+    implicit val spliteratorUnboxInt = new Unboxer[Int, Spliterator, Spliterator.OfInt] {
+      def apply(boxed: Spliterator[Int]) = new UnboxedIntSpliterator(boxed)
+    }
+    implicit val spliteratorUnboxLong = new Unboxer[Long, Spliterator, Spliterator.OfLong] {
+      def apply(boxed: Spliterator[Long]) = new UnboxedLongSpliterator(boxed)
+    }
+    implicit val jstreamUnboxDouble = new Unboxer[Double, ObjectStream, DoubleStream] {
+      def apply(boxed: ObjectStream[Double]): DoubleStream = boxed.mapToDouble(x => x)
+    }
+    implicit val jstreamUnboxInt = new Unboxer[Int, ObjectStream, IntStream] {
+      def apply(boxed: ObjectStream[Int]): IntStream = boxed.mapToInt(x => x)
+    }
+    implicit val jstreamUnboxLong = new Unboxer[Long, ObjectStream, LongStream] {
+      def apply(boxed: ObjectStream[Long]): LongStream = boxed.mapToLong(x => x)
+    }
+  }
 }
 
-package object spec {
+package object spec extends Java8StreamImplicits {
 }
